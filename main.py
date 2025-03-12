880515b97466b2d16f432a627aeb1f3a3dd18a53
programmeroexe <stupidprogrammer100@gmail.com>
2025-03-13 01:15:59 +0530

    whats vision?

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+WHAT THIS SOFTWARE WILL BE ABLE TO:
+speak
+log my weight and stuff like that
+log my irl goals
+log my irl study goals
+log my audio as dairy or somethng else
+log my text into audio as dairy or somethng else
+act as a dairy 
+act as motivation
+idk stuff like that
+"""
