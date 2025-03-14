ec1cd369633dd565fb3cedd698596152f841a2b6
programmeroexe <stupidprogrammer100@gmail.com>
2025-03-14 19:32:13 +0530

    -fixed a small error

--- urwid_learning/display text.py
+++ urwid_learning/display text.py
@@ -9,8 +9,8 @@ def exit_on_q(key):
 text = urwid.Text("CENTERED TEXT",align="center")
 text2 = urwid.Text("LEFT TEXT", align="left")
 text3 = urwid.Text("RIGHTENED TEXT", align="right")
-# pilee = urwid.Pile([text,text2,text3])
-fill = urwid.Filler()
+pilee = urwid.Pile([text,text2,text3])
+fill = urwid.Filler(pilee)
 
 loop = urwid.MainLoop(fill, unhandled_input=exit_on_q)
 loop.run()
