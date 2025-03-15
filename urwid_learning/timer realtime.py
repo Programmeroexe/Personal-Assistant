f37843ae5d071fda6eb2aadf1061f18e02f56b8d
programmeroexe <stupidprogrammer100@gmail.com>
2025-03-15 17:21:51 +0530

    chatgpt version

--- urwid_learning/timer realtime.py
+++ urwid_learning/timer realtime.py
@@ -1,25 +1,25 @@
 import urwid
 from pyfiglet import Figlet
 
+count = 0  # No need for global now
 
-count = 0
 def q(k):
-    if k in ('q',"Q"):
+    if k in ('q', 'Q'):
         raise urwid.ExitMainLoop()
+
 def ascii_text(text, font="small", width=120):
-    f = Figlet(font=font,width=width)
+    f = Figlet(font=font, width=width)
     return str(f.renderText(str(text)))
 
 def changing(loop, user_data=None):
-    text.set_text(ascii_text(str(count+1)))
-    count += 1
-    loop.set_alarm_in(1,changing)
+    global count  # Use global to modify outside variable
+    count += 1  # Increment count
+    text.set_text(ascii_text(count))  # Update UI
+    loop.set_alarm_in(1, changing)  # Reschedule function
 
-
-text = urwid.Text(ascii_text(count),"center")
+text = urwid.Text(ascii_text(count), "center")
 filling = urwid.Filler(text, "middle")
 
-
 loop = urwid.MainLoop(filling, unhandled_input=q)
-loop.set_alarm_in(1,changing)
+loop.set_alarm_in(1, changing)  # Start updating
 loop.run()
