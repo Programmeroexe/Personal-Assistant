fc02b7ae088bbdc2428c6602786f3d34e9a07011
programmeroexe <stupidprogrammer100@gmail.com>
2025-03-15 15:35:30 +0530

    fixed seleciton problem

--- urwid_learning/radio_button.py
+++ urwid_learning/radio_button.py
@@ -1,18 +1,19 @@
 import urwid
+import time
 def on_q(k):
     if k in ('q','Q'):
         raise urwid.ExitMainLoop()
 
 def clicked(button, state, user_data):
-    text1.set_text(f"button: {button.label}\nstate:{state}\nuser_data: {user_data}")
+    if state:
+        text1.set_text(button.label)
 
 group = []
 radio_button1 = urwid.RadioButton(group, "radio button 1", True, on_state_change=clicked,user_data="RB1")
-radio_button2 = urwid.RadioButton(group, "radio button 2", False, on_state_change=clicked,user_data="RB2")
-radio_button3 = urwid.RadioButton(group, "radio button 3", False, on_state_change=clicked,user_data="RB3")
+radio_button2 = urwid.RadioButton(group, "radio button 2", on_state_change=clicked,user_data="RB2")
+radio_button3 = urwid.RadioButton(group, "radio button 3", on_state_change=clicked,user_data="RB3")
 string= ""
-for i in range(len(group)):
-    string= string+str(group[i])+"\n"
+
 text1 = urwid.Text(string)
 wrapping = urwid.Padding(urwid.Pile([radio_button1,radio_button2,radio_button3,text1]),align="center")
 
