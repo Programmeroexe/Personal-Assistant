17f6df8e5d72d10f5a5ea9a2efbeadd5ebfa2671
programmeroexe <stupidprogrammer100@gmail.com>
2025-03-16 15:33:57 +0530

    tiemr realtime with ascii 👍🏼

--- urwid_learning/timer realtime.py
+++ urwid_learning/timer realtime.py
@@ -1,25 +1,26 @@
 import urwid
 from pyfiglet import Figlet
 
-count = 0  # No need for global now
 
+count = 0.0
 def q(k):
-    if k in ('q', 'Q'):
+    if k in ('q',"Q"):
         raise urwid.ExitMainLoop()
-
-def ascii_text(text, font="small", width=120):
-    f = Figlet(font=font, width=width)
+def ascii_text(text, font="big", width=120):
+    f = Figlet(font=font,width=width)
     return str(f.renderText(str(text)))
 
 def changing(loop, user_data=None):
-    global count  # Use global to modify outside variable
-    count += 1  # Increment count
-    text.set_text(ascii_text(count))  # Update UI
-    loop.set_alarm_in(1, changing)  # Reschedule function
+    global count
+    count += 1
+    text.set_text(ascii_text(str(round(count,3))))
+    loop.set_alarm_in(1,changing)
 
-text = urwid.Text(ascii_text(count), "center")
+
+text = urwid.Text(ascii_text(count),"center")
 filling = urwid.Filler(text, "middle")
 
+
 loop = urwid.MainLoop(filling, unhandled_input=q)
-loop.set_alarm_in(1, changing)  # Start updating
+loop.set_alarm_in(1,changing)
 loop.run()
